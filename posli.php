91a0f06b5720a1d28625ee3caa6e2d364078335b
pavel281185 <pavelkvasnicka@gmail.com>
2023-01-06 22:52:34 +0100

    sémantivké úpravy, změna písma

--- posli.php
+++ posli.php
@@ -1,11 +1,69 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=windows-1250">
-<title>Odesílání emailu</title>
-</head>
-<body>
-<h1>Odesílání emailu</h1>
+<!-- <script src="head.js"></script> -->
+<!-- <script src="header-navigation.js"></script> -->
+<!DOCTYPE html>
+<html lang="cs">
+  <!-- Head -->
+  <head>
+    <!-- Main head settings -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Pavel Kvasnička" />
+    <meta name="keywords" content="Pavel, Kvasnička, prezentace, osobní" />
+    <meta name="robots" content="index,follow" />
+    <meta
+      name="description"
+      content="Pavel Kvasnička - osobní webové stránky."
+    />
+    <meta http-equiv="refresh" content="2";url="index.html">
+    <!-- Linked favicon -->
+    <link rel="shortcut icon" href="./ico/favicon.png" type="image/x-icon" />
+    <!-- Linked CSS -->
+    <link rel="stylesheet" href="css/general.css" />
+    <link rel="stylesheet" href="css/header.css" />
+    <link rel="stylesheet" href="css/navigation.css" />
+    <link rel="stylesheet" href="css/content.css" />
+    <link rel="stylesheet" href="css/footer.css" />
+    <!-- Linked font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Rubik:wght@400;500;700&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Linked Lightbox files -->
+    <link rel="stylesheet" type="text/css" href="css/lightbox.min.css" />
+    <script src="js/lightbox-plus-jquery.min.js"></script>
+    <!-- Title -->
+    <title>Pavel Kvasnička</title>
+  </head>
+  <!-- Body -->
+  <body>
+    <!-- <div class="main-container"> -->
+    <div class="header">
+      <!-- Header -->
+      <header class="main-header" onclick="location.href='./index.html';">
+        <h1 class="header-text">Pavel Kvasnička</h1>
+      </header>
+      <!-- Navigation -->
+      <nav class="main-nav">
+        <ul class="nav">
+          <li class="nav-item" onclick="location.href='./omne.html';">O mně</li>
+          <li class="nav-item" onclick="location.href='./praxe.html';">
+            Praxe
+          </li>
+          <li class="nav-item" onclick="location.href='./fotogalerie.html';">
+            Fotogalerie
+          </li>
+          <li class="nav-item" onclick="location.href='./kontakty.html';">
+            Kontakty
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <!-- Main content -->
+    <div class="master">
+      <main class="main-article">
 <?php
 $jmeno = $_POST['jmeno'];
 $email = $_POST['email'];
@@ -22,5 +80,17 @@ else
 echo "<p>Váš e-mail se <strong>nepodařilo odeslat</strong> pravděpodobně jste nevyplnili všechny údaje, nebo nevíte kolik je 1 + 1.</p>";
 }
 ?>
-</body>
+</main>
+    </div>
+    <!-- Footer -->
+    <footer class="main-footer">
+      &copy;<span id="year">2021</span>, Pavel Kvasnička,
+      pavelkvasnicka@gmail.com
+      <script>
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+      </script>
+    </footer>
+    <!-- </div> -->
+  </body>
 </html>
+<!-- <script src="footer.js"></script> -->
